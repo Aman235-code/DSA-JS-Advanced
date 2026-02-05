4a9a1bd6d98399deba7ea2adeb7f302ea8044cf7
Aman Ahamed <amanahamed04@gmail.com>
2026-02-05 12:46:26 +0530

    setZeroes

---
+++ advanced/leetcode/setZeroes.js
@@ -0,0 +1,49 @@
+var setZeroes = function (matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  let firstRowHasZero = false;
+  let firstColHasZero = false;
+
+  for (let j = 0; j < cols; j++) {
+    console.log(matrix[0][j]);
+    if (matrix[0][j] == 0) firstRowHasZero = true;
+  }
+
+  for (let i = 0; i < rows; i++) {
+    if (matrix[i][0] == 0) firstColHasZero = true;
+  }
+
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      if (matrix[i][j] == 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  if (firstRowHasZero) {
+    for (let j = 0; j < cols; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+
+  if (firstColHasZero) {
+    for (let i = 0; i < rows; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  return matrix;
+};
+
+console.log(setZeroes([[1, 0, 3]]));
